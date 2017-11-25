6e644014d84b0f7f4d6ddb5d0d066fd29175dfcf
Victor <deep-research>
2017-11-24 21:37:40 -0600

    Prototype methods

--- letter-constructor.js
+++ letter-constructor.js
@@ -1,21 +1,26 @@
 // The Letter constructor
 var Letter = function(letter) {
-    
     // The letter value
     this.value = letter;
 
     // If the letter is guessed, the status becomes true
     this.guessedStatus = false;
+}
 
-    // This function updates the guessed status
-    this.guessedCorrectly = function() {
-        this.guessedStatus = true;
-    };
+// This function updates the guessed status
+Letter.prototype.guessedCorrectly = function() {
+    this.guessedStatus = true;
+}
 
+Letter.prototype.display = function() {
     // The letter appears after it has been guessed
     this.display = function() {
+
+        // Display the letter if the letter has been guessed
         if (this.guessedStatus) {
             return this.value
+
+        // Display an underscore in the letter is still hidden
         } else {
             return "_"
         }
--- word-constructor.js
+++ word-constructor.js
@@ -5,8 +5,7 @@ var letterConstructor = require('./letter-constructor')
 
 // the word constructor
 var Word = function() {
-
-    // array of possible word choices
+    // Array of possible word choices
     this.wordList = ["apple", "pineapple", "pear", "bannana", "watermelon", "orange", "tangerine", "kiwi", "coconut"];
 
     // the word that has been chosen
@@ -14,67 +13,66 @@ var Word = function() {
 
     // The letters in the word as an array of objects
     this.letterObjs = [];
+}
 
-    // choose a new word
-    this.chooseWord = function() {
+// choose a new word
+Word.prototype.chooseWord = function() {      
+    // generate an index number between zero and the length of wordList - 1
+    var randInt = randomInt(this.wordList.length - 1);
 
-        // generate an indexnnumber between zero and the length of wordList - 1
-        var randInt = randomInt(this.wordList.length - 1);
+    // retrieve the word by index number from the wordList array
+    var newWord = this.wordList[randInt];
 
-        // retrieve the word by index number from the wordList array
-        var newWord = this.wordList[randInt];
+    // Update the word as the chosen word
+    this.chosenWord = newWord;     
 
-        // Update the word as the chosen word
-        this.chosenWord = newWord;     
+    // Generate an array with the letters in the word
+    var letterArray = this.chosenWord.split("")
 
-        // Generate an array with the letters in the word
-        var letterArray = this.chosenWord.split("")
+    // For each letter in the letter array
+    for (i=0; i<letterArray.length; i++) {
 
-        // For each letter in the letter array
-        for (i=0; i<letterArray.length; i++) {
-            
-            // convert to lowercase and create an object
-            letterObj = new letterConstructor(letterArray[i].toLowerCase())
+        // convert to lowercase and create an object
+        letterObj = new letterConstructor(letterArray[i].toLowerCase())
 
-            // save to an array of letter objects
-            this.letterObjs.push(letterObj)
-        }
-    };
+        // save to an array of letter objects
+        this.letterObjs.push(letterObj)
+    }
+}
+        
+// update the game status
+Word.prototype.gameStatus = function() {
+    var letterCount = 0;
 
-    // update the game status
-    this.gameStatus = function() {
-        var letterCount = 0;
-
-        // add to the count for every letter that has been guessed
-        for (i=0; i<this.letterObjs.length; i++) {
-            var letterObj = this.letterObjs[i]
-            if (letterObj.guessedStatus) {
-                letterCount++;
-            }
-        }
-
-        // if all the letters were guessed, update the game status to true
-        if (letterCount === this.letterObjs.length) {
-            return true;
+    // add to the count for every letter that has been guessed
+    for (i=0; i<this.letterObjs.length; i++) {
+        var letterObj = this.letterObjs[i]
+        if (letterObj.guessedStatus) {
+            letterCount++;
         }
     }
 
-    // Display the latest version of the word
-    this.printLetters = function() {
-        letterArray = []
-        for (i=0; i<this.letterObjs.length; i++) {
-            letterArray.push(this.letterObjs[i].display())
-        }
-        console.log(letterArray.join(" ") + "\n")
+    // if all the letters were guessed, update the game status to true
+    if (letterCount === this.letterObjs.length) {
+        return true;
     }
-
-    // Reset the object and choose a new word
-    this.reset = function(){
-        this.chosenWord = "";
-        this.letterObjs = [];
-
-        this.chooseWord()
+}
+        
+// Display the latest version of the word
+Word.prototype.printLetters = function() {
+    letterArray = []
+    for (i=0; i<this.letterObjs.length; i++) {
+        letterArray.push(this.letterObjs[i].display())
     }
+    console.log(letterArray.join(" ") + "\n")
+}
+        
+// Reset the object and choose a new word
+Word.prototype.reset = function(){
+    this.chosenWord = "";
+    this.letterObjs = [];
+
+    this.chooseWord()
 }
 
 // Export the constructor to the hangman.js file
